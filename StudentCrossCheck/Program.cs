8e196df8976ad19e0b0d546014d4aa4f055a612e
JASON BROWN <JASBROWN@uat.edu>
2018-09-20 15:09:14 -0700

    Add project files.

---
+++ StudentCrossCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Console = System.Console;
+
+namespace StudentCrossCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Student Cross Check 1.0";
+            Console.WriteLine("-");
+            try
+            {
+                StreamReader sr = new StreamReader(File.Create("students.txt"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
